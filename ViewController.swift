fd6abf87c689151d7a52dfbeb624495aa3f3fdd3
Apple <apple@Apples-MacBook-Pro-88.local>
2019-07-12 10:37:26 -0500

    Iteration 1

--- ViewController.swift
+++ ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func showMessage (sender: UIButton) {
+     
+        //pop up
+        //fortune
+        //pop up goes away after they press a ok button
+        let alertController = UIAlertController(title: "Fortune", message: "You will get a private Harry Styles concert.", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
