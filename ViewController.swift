4606665c520853b5992741742d153f50cf22c1ee
bela.gadsby@gmail.com <apple@Apples-MacBook-Pro-88.local>
2019-07-12 11:31:28 -0500

    Iteration2

--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let emojis = ["🥠": "FORTUNE", "⭐️": "LUCKY NUMBER"]
+    let showMessages = [
+        "FORTUNE" : ["Harry Styles will ask you out." , "You will move to Greece." , "You will win $1,000,000." , "You will drop your ice cream."],
+        "LUCKY NUMBER" : [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ]]
+    
+    
     @IBAction func showMessage (sender: UIButton) {
+        let selectedEmotion =
+        sender.titleLabel?.text
      
         //pop up
         //fortune
         //pop up goes away after they press a ok button
-        let alertController = UIAlertController(title: "Fortune", message: "You will get a private Harry Styles concert.", preferredStyle: UIAlertController.Style.alert)
+        let alertController =
+            UIAlertController(title: "FORTUNE OR LUCKY NUMBER", message: "", preferredStyle: UIAlertController.Style.alert)
     alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
